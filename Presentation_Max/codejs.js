c2855df6d2eb8fb45cc68206ab837803b7aefb8b
MrAskone <antoine.sako.kone@gmail.com>
2017-08-22 16:51:07 +0200

    Cours du 22 Aout et exos

---
+++ Presentation_Max/codejs.js
@@ -0,0 +1,10 @@
+function f(){
+      // var t = []
+      var t = ["bonjour a toi","Antoine","je","suis"]
+
+      t.sort(function(a,b){return a.length-b.length})
+
+      console.log(t)
+  }
+alert("\<strong\>Je suis une bete!!!!!\</strong\>")
+document.write("<h2>bonjour</h2>")
