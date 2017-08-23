fb606d44805ca9c6ce8a98c1fe3c72cd2fc499ad
MrAskone <antoine.sako.kone@gmail.com>
2017-08-23 10:53:06 +0200

    exo 5 completed

--- Exercice 5/exo5.js
+++ Exercice 5/exo5.js
@@ -1,10 +1,22 @@
 
-function cache()
+function cache(id)
 {
-  document.getElementById('postIt').style.visibility = "hidden"
+  document.getElementById(id).style.visibility = "hidden"
+  document.getElementById(id).innerHTML = "invisible"
 }
 
-function affiche()
+function affiche(id)
 {
-  document.getElementById('postIt').style.visibility = "visible"
+  document.getElementById(id).style.visibility = "visible"
+  document.getElementById(id).innerHTML = "Vous avez rendu la boite visible"
+}
+
+function textHover(id)
+{
+  document.getElementById(id).innerHTML = "Abracadabra! Quand on survole le paragraphe, un nouveau texte s'affiche!"
+}
+
+function textHoverEnd(id)
+{
+  document.getElementById(id).innerHTML = "Bye bye!"
 }
