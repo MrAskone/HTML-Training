58593e249a85cf8919672aa7c891298fcc012dd6
MrAskone <antoine.sako.kone@gmail.com>
2017-08-26 19:12:17 +0200

    Exercice 9 ajouté et fini

---
+++ Exercice 9/exo9.js
@@ -0,0 +1,11 @@
+document.getElementById("codeNameApplication").value =
+"navigator.appCodeName is " + navigator.appCodeName
+
+document.getElementById("nomApplication").value =
+"navigator.appName is " + navigator.appName
+
+document.getElementById("versionApplication").value =
+navigator.appVersion
+
+document.getElementById("userAgent").value =
+navigator.userAgent;
